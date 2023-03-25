af3e6a3c4142eac273148b96c01890911ebd874c
Nika <nikalomiashvili25@gmail.com>
2023-03-25 13:36:28 +0400

    portfolio-project

---
+++ src/components/helpers/projectsList.js
@@ -0,0 +1,18 @@
+import project01 from "./../../img/projects/01.png";
+
+
+
+
+const projects = [
+    {
+        title: 'Car Buying',
+        skills: 'Angular, TypeScript Node.js',
+        img: project01,  
+        imgBig: project01,
+        githubLink: 'https://nikal25.github.io/cars-hw/',
+    },
+  
+
+]
+
+export {projects};
